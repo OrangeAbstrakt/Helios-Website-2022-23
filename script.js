1fa7a26977a5c11280a69e0e146b6623463c5971
ORNAGE ABSTRAKT <divjeet.asb@gmail.com>
2022-10-02 19:11:15 +0530

    Added Hamburger menu and fixed bugs

---
+++ script.js
@@ -0,0 +1,17 @@
+function rNavBar() {
+  document.getElementById('ham').style.display = 'block';
+  document.getElementById('ham-btn').style.display = "block";
+  document.getElementById('ham-1').style.display = "block";
+  document.getElementById('ham-2').style.display = "block";
+  document.getElementById('ham-3').style.display = "block";
+}
+
+
+// var w = window.innerWidth;
+
+// if (w < 480){
+//   document.getElementById('rnavbtn').style.display = 'block';
+// } else {
+//   document.getElementById('rnavbtn').style.display = 'none'
+// }
+
